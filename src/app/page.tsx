cb9bba5aae3818cf0501203f8ee51660ece35141
ImInBox <iminbox24dev@gmail.com>
2024-10-01 14:47:36 +0200

    add github to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="p-8">
-      <Link href={"01"} className="hover:underline">
+    <main className="flex flex-col p-8">
+      <a
+        href="https://github.com/liliankaa/bartusiowe_zadania"
+        className="text-2xl hover:underline"
+      >
+        Github
+      </a>
+      <Link href={"01"} className="mt-4 hover:underline">
         Zadanie - 01
       </Link>
     </main>
